5fc520a3936fa191a2685524d7d6be013a555cc4
YagoSchipper <6022749@mborijnland.nl>
2024-01-08 12:32:25 +0100

    oh my gyatt

---
+++ RoomieReserveMAUI/Components/Data/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomieReserveMAUI.Components.Data
+{
+    public class Color
+    {
+        string color {  get; set; }
+    }
+}
