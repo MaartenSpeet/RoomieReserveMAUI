47588faec5373d0fd79aab8e3108e088c57834de
YagoSchipper <6022749@mborijnland.nl>
2024-01-08 11:50:15 +0100

    update

---
+++ RoomieReserveMAUI/Components/Data/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomieReserveMAUI.Components.Data
+{
+    public class Item
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public int Owner_Id { get; set; }
+        public int Boxie_Id { get; set; }
+        public DateOnly BestBefore { get; set; }
+    }
+}
